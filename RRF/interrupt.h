699548f3c8192d2b2fe4091af99968b0b7d45ac6
francolmenar <correofalso97@hotmail.es>
2018-03-12 05:30:58 -0400

    Initial commit

---
+++ RRF/interrupt.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <ucontext.h>
+#include <unistd.h>
+
+
+#include <time.h>
+
+
+
+#define TICK_TIME 5000
+#define PACK_TIME 1
+#define STARVATION 200
+
+static sigset_t maskval_interrupt,oldmask_interrupt;
+
+void timer_interrupt ();
+void init_interrupt();
+void disable_interrupt();
+void enable_interrupt();
+
+void network_interrupt ();
+void init_network_interrupt();
+void disable_network_interrupt();
+void enable_network_interrupt();

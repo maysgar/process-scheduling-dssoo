d11ccdf46455d64adf5dbb0a91d9e1139929238b
francolmenar <correofalso97@hotmail.es>
2018-03-13 04:26:47 -0400

    RR limpio limpito

--- RR/RR.c
+++ RR/RR.c
@@ -81,65 +81,6 @@ void init_mythreadlib() {
 	init_interrupt();
 }
 
-/*Initialize the queue of processes*/
-void init_RR(){
-	tqueue = queue_new(); //I initialize the queue
-	TCB aux;
-
-	/* Create context for the idle thread */
-	if(getcontext(&idle.run_env) == -1)
-	{
-		perror("*** ERROR: getcontext in init_thread_lib");
-		exit(-1);
-	}
-
-	idle.state = IDLE;
-	idle.priority = SYSTEM;
-	idle.function = idle_function;
-	idle.run_env.uc_stack.ss_sp = (void *)(malloc(STACKSIZE));
-	idle.tid = -1;
-
-	if(idle.run_env.uc_stack.ss_sp == NULL)
-	{
-		printf("*** ERROR: thread failed to get stack space\n");
-		exit(-1);
-	}
-
-	idle.run_env.uc_stack.ss_size = STACKSIZE;
-	idle.run_env.uc_stack.ss_flags = 0;
-	idle.ticks = QUANTUM_TICKS;
-
-	makecontext(&idle.run_env, idle_function, 1);
-
-	aux.state = INIT;
-	aux.priority = LOW_PRIORITY;
-	aux.ticks = QUANTUM_TICKS;
-	aux.state = FREE;
-	aux.tid = 0;
-
-	t_state[0].state = INIT;
-	t_state[0].tid = 0;
-
-
-	if(getcontext(&aux.run_env) == -1)
-	{
-		perror("*** ERROR: getcontext in init_thread_lib");
-		exit(5);
-	}
-
-	for(int i=1; i<N; i++){
-		t_state[i].state = FREE;
-	}
-
-	printf("%i\n", aux.tid);
-	running = &aux;
-	printf("%i\n", running -> tid);
-
-	/* Initialize network and clock interrupts */
-	init_network_interrupt();
-	init_interrupt();
-}
-
 /*It blocks all the signals
  * Return 0 in success and -1 otherwise*/
 int blockSignals(){
@@ -163,7 +104,7 @@ int mythread_create (void (*fun_addr)(),int priority)
 
 	if (!init) /*if the library is not initialized*/
 	{
-		init_RR(); /*init the queue of threads*/
+		init_mythreadlib(); /*init the queue of threads*/
 		init=1;
 	}
 	for (i=0; i<N; i++) /*search for a free position in the thread array*/
@@ -204,49 +145,6 @@ int mythread_create (void (*fun_addr)(),int priority)
 	printf("\t\tQueue after inserting\n");
 	queue_print(tqueue);
 	return i;
-} /****** End my_thread_create() ******/
-
-/* Problemas con el puto puntero*/
-int mythread_createRR (void (*fun_addr)(),int priority)
-{
-	//int i;
-	TCB aux;
-
-	if (!init) /*if the library is not initialized*/
-	{
-		//init_mythreadlib();
-		init_RR(); /*init the queue of threads*/
-		init=1;
-	}
-	/*initialize the context of the thread which is going to be created*/
-	if(getcontext(&aux.run_env) == -1)
-	{
-		perror("*** ERROR: getcontext in my_thread_create");
-		exit(-1);
-	}
-
-	/*initialize the fields of the thread*/
-	aux.state = INIT;
-	aux.priority = priority; /*given by input to this method*/
-	aux.function = fun_addr; /*given by input to this method*/
-	aux.run_env.uc_stack.ss_sp = (void *)(malloc(STACKSIZE));
-	if(aux.run_env.uc_stack.ss_sp == NULL)
-	{
-		printf("*** ERROR: thread failed to get stack space\n");
-		exit(-1);
-	}
-	aux.tid = idCount++;
-	aux.run_env.uc_stack.ss_size = STACKSIZE;
-	aux.run_env.uc_stack.ss_flags = 0;
-	aux.ticks = 3; /*inventado por mi*/
-
-	blockSignals(); /*block the signals while using the queue*/
-	enqueue(tqueue, &aux); /*enqueue the thread in the queue of threads*/
-	unlockSignals(); /*Unlock the signals*/
-	makecontext(&aux.run_env, fun_addr, 1); /*create the new context*/
-	printf("\t\tQueue after inserting\n");
-	queue_print(tqueue);
-	return idCount;
 } /****** End my_thread_create() ******/
 
 /* Read network syscall */

34b5e161f245e5cc3d5caf8b7827df6fc3840aea
Ismael Garrido <100346215@alumnos.uc3m.es>
2018-03-18 14:28:41 +0100

    print simple

--- RRF/main.c
+++ RRF/main.c
@@ -7,7 +7,7 @@ void fun1 (int global_index)
 	int a=0, b=0;
 	read_network();
 	for (a=0; a<10; ++a) {
-//    printf ("Thread %d with priority %d\t from fun2 a = %d\tb = %d\n", mythread_gettid(), mythread_getpriority(), a, b);
+    printf ("Thread %d with priority %d\t from fun2 a = %d\tb = %d\n", mythread_gettid(), mythread_getpriority(), a, b);
 		for (b=0; b<25000000; ++b);
 	}
 
